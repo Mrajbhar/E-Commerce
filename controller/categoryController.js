d75c49daf2fbac31e5283968d9d88b9fafd1f926
Mrajbhar <m.rajbhar1235@gmail.com>
2024-02-22 00:21:01 +0530

    new commit

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -37,3 +37,74 @@ export const createCategoryController = async (req,res) =>{
         })
     }
 };
+
+//update category
+
+export const updateCategoryController = async (req,res) =>{
+
+    try {
+
+        const {name} = req.body;
+        const {id} = req.params;
+        const category = await CategoryModel.findByIdAndUpdate(id,{name,slug:slugify(name)},{new:true});
+        res.status(200).send({
+            success:true,
+            message:"Category Updated Successfully",
+            category,
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            error,
+            message:"Error while updating category"
+        });
+    }
+};
+
+//get all category
+
+export const categoryController = async (req,res) =>
+{
+    try {
+        const category = await CategoryModel.find({});
+        res.status(200).send({
+            success:true,
+            message:"All Categories List",
+            category,
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            error,
+            message:"Error while getting all category"
+        });
+    }
+};
+
+//single category controller
+
+
+export const singleCategoryController = async (req,res) =>
+{
+    try {
+
+        const category = await CategoryModel.findOne({slug:req.params.slug});
+        res.status(200).send({
+            success:true,
+            message:"Get Single Category Successfully",
+            category,
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            error,
+            message:"Error while getting single category"
+        });
+    }
+}
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,13 +1,25 @@
 import express from "express";
 import { RequireSignin, isAdmin } from './../middlewares/authoMiddlerware.js';
-import { createCategoryController } from "../controller/categoryController.js";
+import { categoryController, createCategoryController, singleCategoryController, updateCategoryController } from "../controller/categoryController.js";
 
 
 const router = express.Router();
 
 //Routers
-
+//create category
 router.post("/create-category",RequireSignin,isAdmin,createCategoryController);
+
+//update category
+
+router.put("/update-category/:id",RequireSignin,isAdmin,updateCategoryController)
+
+
+//getAll category
+
+router.get("/get-category",categoryController);
+
+//single category
+router.get("/single-category/:slug",singleCategoryController);
 
 
 export default router;
