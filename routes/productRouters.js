4023f9b4c4497035242b354d1c428d04ebbc3c7d
Mrajbhar <m.rajbhar1235@gmail.com>
2024-02-28 00:00:03 +0530

    new changes

--- routes/productRouters.js
+++ routes/productRouters.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { RequireSignin, isAdmin } from "../middlewares/authoMiddlerware.js";
-import { createProductController, getProductController, getSingleProductController } from "../controller/productController.js";
+import { createProductController, deleteProductController, getProductController, getSingleProductController, productPhotoController, updateProductController } from "../controller/productController.js";
 import ExpressFormidable from "express-formidable";
 
 const router = express.Router();
@@ -15,7 +15,24 @@ router.get("/get-product",getProductController);
 
 //Single get product
 
-router.get("/get-product/slug",getSingleProductController);
+router.get("/get-product/:slug", getSingleProductController);
 
+//get photo
+router.get("/product-photo/:pid", productPhotoController);
+
+
+
+//delete rproduct
+router.delete("/delete-product/:pid",deleteProductController);
+
+
+// update product
+router.put(
+    "/update-product/:pid",
+    RequireSignin,
+    isAdmin,
+    ExpressFormidable(),
+    updateProductController
+  );
 
 export default router
