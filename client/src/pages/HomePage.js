8a4a0ff0a705ed077b19fee6ff2962685fe86a4a
Mrajbhar <m.rajbhar1235@gmail.com>
2024-03-07 08:31:32 +0530

    new changes on products

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Layout from '../components/Layout/Layout';
 import { useAuth } from '../context/auth';
+import { useNavigate } from "react-router-dom";
+
 
 const HomePage = () => {
-  const [auth,setAuth] = useAuth();
+  const navigate = useNavigate();
+  // const [cart, setCart] = useCart();
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  // const [checked, setChecked] = useState([]);
+  // const [radio, setRadio] = useState([]);
+  // const [total, setTotal] = useState(0);
+  // const [page, setPage] = useState(1);
+  // const [loading, setLoading] = useState(false);
 
   return (
-    <Layout title={"HomePage"}>
-        <h1>HomePage</h1>
-        <pre>{JSON.stringify(auth,null,4)}</pre>
+    <Layout title={"All Products -Best offers"}>
+
+      <div className="row">
+        <div className="col-md-3">
+          <h6 className="text-center">Filter By Category</h6>
+
+        </div>
+        <div className="col-md-9">
+          <h1 className="text-center"> All Products</h1>
+          </div>?
+      </div>
+       
     </Layout>
   );
 };
